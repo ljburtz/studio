a0f1dde2d84831ce7a4b2bdf49a1436d20e17935
Andrew Carr <andrew@foxglove.dev>
2022-07-22 10:49:04 +1000

    Update Interaction tool import\/exports to be more sensible and handle click away action

--- packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
+++ packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
@@ -49,7 +49,7 @@ import ThemeProvider from "@foxglove/studio-base/theme/ThemeProvider";
 import { Point, makeCovarianceArray } from "@foxglove/studio-base/util/geometry";
 
 import { DebugGui } from "./DebugGui";
-import Interactions, { InteractionContextMenu, SelectionObject, TabType } from "./Interactions";
+import { Interactions, InteractionContextMenu, SelectionObject, TabType } from "./Interactions";
 import type { Renderable } from "./Renderable";
 import { MessageHandler, Renderer, RendererConfig } from "./Renderer";
 import { RendererContext, useRenderer, useRendererEvent } from "./RendererContext";
@@ -62,6 +62,7 @@ import { Pose } from "./transforms/geometry";
 const log = Logger.getLogger(__filename);
 
 const SHOW_DEBUG: true | false = false;
+
 const PANEL_STYLE: React.CSSProperties = {
   width: "100%",
   height: "100%",
@@ -341,6 +342,7 @@ function RendererOverlay(props: {
       </div>
       {clickedObjects.length > 1 && !selectedObject && (
         <InteractionContextMenu
+          onClose={() => setSelectedRenderables([])}
           clickedPosition={clickedPosition}
           clickedObjects={clickedObjects}
           selectObject={(selection) => {
